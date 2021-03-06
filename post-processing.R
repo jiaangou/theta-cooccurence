740c7e7dcdbd9ab3c2503d240e9cbaa4572e21f0
VulpiaMicrostachys <wou@zoology.ubc.ca>
2021-03-05 19:32:42 -0800

    update processing script

--- post-processing.R
+++ post-processing.R
@@ -15,7 +15,7 @@ optima <- c('uniform','normal', 'skew')
 
 #second level: parallel simulations (4 simulations)
 sim[[1]]%>%length
-iterations <- 1:4
+simulation <- 1:4
 
 
 #third level: `mcomsimr` simulation outputs (dynamics.df, landscape, env.df, env_traits, disp_mat, interaction)
@@ -26,16 +26,16 @@ dynamics.df.all <- list()
 
 for (i in 1:length(optima)){
   
-  dynamics.df.iterations <- list()
-  for (j in iterations){
+  dynamics.df.simulations <- list()
+  for (j in simulation){
     #extract each iteration, label, and put in a list
-    dynamics.df.iterations[[j]] <- sim[[i]][[j]]$dynamics.df%>%
+    dynamics.df.simulations[[j]] <- sim[[i]][[j]]$dynamics.df%>%
       mutate(optima = optima[i])%>%
-      mutate(iteration = j)
+      mutate(simulation = j)
     
   }
   #combine the labeled iteration dfs into a single df by binding rows
-  dynamics.df.all[[i]] <- dynamics.df.iterations%>% 
+  dynamics.df.all[[i]] <- dynamics.df.simulations%>% 
     bind_rows()
 
   #name the list elements by optima distribution
@@ -43,7 +43,41 @@ for (i in 1:length(optima)){
   
 }
 
-#COMBINE
+#COMBINE into a nested tibble grouped by optima distribution and simulation 
 dynamics.df.all <- dynamics.df.all%>%
-  bind_rows(.id = 'optima_distribution')
+  bind_rows(.id = 'optima_distribution')%>%
+  group_by(optima_distribution, simulation)%>% 
+  nest()
+
+#Calculate theta for each simulated metacommunity------
+#Ia. convert dynamics.df to wide format (sample x species)
+spe_compo_wide <- function(x){
+  out <- x%>%
+    mutate(species = paste0('sp',species))%>%
+    pivot_wider(id_cols = c(patch, env), names_from = 'species', values_from = N, values_fill = 0) #fills NAs with 0
+  return(out)
+}
+
+dynamics.df.all$data[[1]]%>%
+  spe_compo_wide()
+
+
+#Ib. extract spp (fundamental) parameters (optima, niche width, max_r)
+spe_parameters <- function(x){
+  out <- x%>%
+    select(species, optima, env_niche_breadth, max_r)%>%
+    distinct()
+  return(out)
+}
+
+dynamics.df.all$data[[1]]%>%
+  spe_parameters()
+
+#II. Calculate theta
+devtools::install_github ('zdealveindy/theta')
+library(theta)
+
+
+
+
 
